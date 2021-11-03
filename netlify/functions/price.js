411c7f8a245c65bd592f349ca3c7b7f2f9d5d301
Travis Horn <travis@travishorn.com>
2021-11-03 10:46:42 -0500

    Working product

---
+++ netlify/functions/price.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+
+exports.handler = async function(event, context) {
+  const ids = event.queryStringParameters.ids;
+  const vs_currencies = event.queryStringParameters.vs_currencies;
+
+  const { data: cgRes } = await axios.get(`https://api.coingecko.com/api/v3/simple/price`, {
+    params: {
+      ids,
+      vs_currencies,
+    },
+  });//?ids=${ids}&vs_currencies=${vsCurrencies}`);
+
+  const output = [];
+
+  Object.entries(cgRes).forEach(([cgCoinId, cgCurrencies]) => {
+    const coinObj = {
+      id: cgCoinId,
+    };
+
+    Object.entries(cgCurrencies).forEach(([cgCurrencyId, value]) => {
+      coinObj[cgCurrencyId] = value;
+    });
+
+    output.push(coinObj);
+  });
+  
+  return {
+      statusCode: 200,
+      body: JSON.stringify(output),
+  };
+};
